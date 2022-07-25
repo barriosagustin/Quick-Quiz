01ed257210c84eb13083f6a3129e8c77b342d2db
AgustinBarrios <agusab2000@gmail.com>
2022-07-25 11:01:02 -0300

    Game updates

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const questionCounterText = document.getElementById('questionCounter');
-const scoreText = document.getElementById('score');
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarfull = document.getElementById("progressBarfull");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -9,33 +10,20 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript??",
-        choice1: "<script>",
-        choice2: "<Javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question:
-            "What is the correct sintax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box",
-        choice1: "msgBox('Hello World');",
-        choice2: "AlertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-    }
-];
+let questions = [];
+
+fetch("questions.json").then(res => {
+    console.log(res);
+    return res.json();
+}).then(loadedquestions => {
+    console.log(loadedquestions);
+    questions = loadedquestions;
+    startGame();
+})
+    .catch(err => {
+        console.error(err);
+    })
+
 
 //CONSTANTS <>
 const CORRECT_BONUS = 10;
@@ -51,11 +39,15 @@ startGame = () => {
 getNewQuestion = () => {
 
     if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
         //go to the end page
         return window.location.assign("/end.html");
     }
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress Bar
+    progressBarfull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -100,4 +92,3 @@ incrementScore = num => {
     scoreText.innerText = score;
 }
 
-startGame();
